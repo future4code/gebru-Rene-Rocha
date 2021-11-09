8a8e235f3d7cfe674d1c8b6a87b9511b992f0302
BuildTools <unconfigured@null.spigotmc.org>
2021-11-09 19:13:32 -0300

    Primeiro exercicio de Primeira aula de JavaScript

---
+++ modulo1/variaveis/Aula1/variaveis-gebru/index.js
@@ -0,0 +1,103 @@
+//--------------------------------------------<>------------------------------------//
+//COMENTÁRIOS: Como Fazer?
+
+//Comentário de Linha
+
+ /*
+ E se eu quiser 
+ fazer um comentarios 
+ em bloco
+ */
+//--------------------------------------------<>------------------------------------//
+// CONSOLE.LOG
+console.log("Hello, World!")
+//--------------------------------------------<>------------------------------------//
+// PROMPT: como pegar informações
+
+// PEGAR INFORMAÇÃO E PRINTAR -> console.log(prompt("Qual é o seu nome?"))
+//--------------------------------------------<>------------------------------------//
+////const idade = prompt("Qual a sua idade?")
+//const nome = prompt("Qual o seu nome?")
+
+//const alerta =console.log("Seu nome é", nome, "e tem", idade)
+//alert("Oi,\ " + nome +"\ " + aidade + "\ Seja bem vindo")
+////
+
+//--------------------------------------------<>------------------------------------//
+
+//Variáveis
+// const nome = "something" -> Para valores constantes
+// let idade = "something" -> Para valores variavais(que mudam de valor)
+//const anoCorrente = prompt('Em que ano estamos?')
+//let anoDeNascimento = prompt('Em que ano você nasceu?')
+//let idade = anocorrente-anoDeNascimento
+//console.log("Você tem ",idade, "Anos")
+
+//--------------------------------------------<>------------------------------------//
+
+//Cammel Case
+//nomeUsuario
+
+//Snake Case
+//Nome_Usuario
+
+//--------------------------------------------<>------------------------------------//
+//Tipos de variáveis
+//Numbers -> Numeros
+//Strings -> Textos
+// Boolean -> True or False -> Algebra Booleana Gergoe Boole
+
+//--------------------------------------------<>------------------------------------//
+// -> Exercício 1
+
+//const nome = "Rene" //string
+//const sobrenome = "Rocha" //string
+//let idade = 21 //number
+//let ehEstudante = true //boolean
+
+//console.log("Meu nome completo é", nome, sobrenome, "e tenho", idade, "anos. Sou estudante?", ehEstudante)
+
+//typeof: comando que permite ver o tipo do valor da varíavel
+/*const nomeUser = prompt("Qual o seu nome?")
+let idadeUser = prompt("Qual a sua idade?")
+
+console.log(typeof nomeUser)
+console.log(typeof idadeUser)
+
+//Tudo que o prompt retorna é sempre String
+*/
+//--------------------------------------------<>------------------------------------//
+// Undefined: Tipo que representa a falta de valor de uma variável.
+// em let funciona mas quando acontece em Const ocorre um erro.
+//let cor = undefined
+//let cor // -> não precisa ser atribuido
+//console.log(typeof cor)
+// Null: também representa a falta de valor da varíavel, existen akgynas duferenças entre undefined e null, uma delas é que null precisa ser atribuido
+// exemplo: const variavel = null  -> Precisa ser atribuído pois null é um tipo de object
+//const cor2 = null
+//console.log(typeof cor2)
+//--------------------------------------------<>------------------------------------//
+// -> Exercício 2 
+/*const nomeUser = prompt('Qual é o seu nome completo?')
+const idadeUser = prompt('Qual é a sua idade')
+
+console.log("Variável nomeUser:", typeof nomeUser)
+console.log("Variável idadeUser:", typeof idadeUser)*/
+//--------------------------------------------<>------------------------------------//
+// ->MÉTODO<- Convertendo tipos de Varíaveis ToString() ToNumber() etc...
+// String => ToString()
+
+/*const idadeNumero = 21
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)*/
+
+// Number => Number(Variável)
+/*const alturaTexto = "1.70"
+const alturaNumero = Number(alturaTexto)
+console.log(typeof alturaNumero)*/
+
+
+
+//--------------------------------------------<>------------------------------------//
