df8bc819c5aad045645bfe85a822d74e98c8055b
BuildTools <unconfigured@null.spigotmc.org>
2022-05-31 23:27:39 -0300

    Tinha esquecido o exercicio 2

--- modulo5/Lista Typescript/src/index.ts
+++ modulo5/Lista Typescript/src/index.ts
@@ -1,13 +1,25 @@
 console.log('Hello World')
 
-const players: {  id: number,   name: string,    score: number} [] = [{
+const instrutores: {  id: number,   name: string, age:number} [] = [{
     id:1,
-    name:"alice",
-    score: 536
+    name:"a",
+    age: 22
 },
 {
-    id:1,
-    name:"alice",
-    score: 536
+    id:2,
+    name:"b",
+    age: 29
 }
 ]
+
+const filtrar = (k:number)=>
+{
+let filtrado = [];
+for(let i = 0; i<instrutores.length; i++)
+    if(instrutores[i].id === k){
+        filtrado.push(instrutores[i].name)
+        filtrado.push(instrutores[i].age)
+        
+    }
+return filtrado
+}
